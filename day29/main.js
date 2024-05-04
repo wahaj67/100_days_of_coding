6c6889bd35621931db5f0ff07501c67110c5dab5
wahaj ali <160678510+wahaj67@users.noreply.github.com>
2024-05-05 04:22:42 +0500

    Add files via upload

---
+++ day29/main.js
@@ -0,0 +1,15 @@
+// q 85
+// function findCodePosition(str:string):number{
+// return str.indexOf("code")
+// }
+// console.log(findCodePosition("learn  to code with javascript "))
+// q86
+function hasJavaScript(str) {
+    return str.includes("JavaScript");
+}
+console.log(hasJavaScript("i love coding in java script"));
+// q87
+function extractFirstTenChars(str) {
+    return str.substring(0, 10);
+}
+console.log(extractFirstTenChars('hello, javacriptworld'));
