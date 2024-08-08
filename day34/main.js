fb48a6111a1cc10b6b6092e2317c3612f487a4d9
wahaj ali <160678510+wahaj67@users.noreply.github.com>
2024-08-08 15:56:55 +0500

    Add files via upload

---
+++ day34/main.js
@@ -0,0 +1,48 @@
+"use strict";
+//  **find the suqare root of any number with using Math.sqrt()function
+// * **Q no 100
+// const num:number= Math.sqrt(295)
+// console.log(num)
+// const num:number= Math.sqrt(278)
+// console.log(num)
+//** */ Q 101/
+// function getRandom():number{
+//   return Math.floor(Math.random()*100)+1
+// }
+// // console.log(getRandom())
+// const random:number = Math.floor(Math.random()*129)+12
+// console.log(random)
+// let grades:number[]=[85,93,78,98,64]
+// function addGrades(grade:number):void{
+//     grades.push(grade)
+// }
+// function removeGrades(index:number):void{
+//     grades.splice(index,1)
+// }
+// function updateGrade(index:number,newGrade:number):void{
+//    grades[index]=newGrade
+// }
+// function calculateGrade():number{
+//     const sum = grades.reduce((acc,grade)=>acc+grade,0)
+//     return Math.round(sum/grades.length)
+// }
+// function getHighestGrade():number{
+//     return Math.max(...grades)
+// }
+// function getLowestGrade():number{
+//     return Math.min(...grades)
+// }
+// addGrades(90)
+// removeGrades(2)
+// updateGrade(1,85)
+// console.log("grades",grades)
+// console.log("average grade",calculateGrade())
+// console.log("highest grade",getHighestGrade())
+// console.log("lowest grade",getLowestGrade())
+// let num:number[]=[384884,38239823,44444444444444,50000,16,3,0]
+// const m = Math.max(...num)
+// const n = Math.min(...num)
+// console.log(m)
+// console.log(n)
+const num = Math.abs(-5 - 15);
+console.log(num);
---
+++ day34/main.ts
@@ -0,0 +1,40 @@
+//  **find the suqare root of any number with using Math.sqrt()function
+// * **Q no 99
+// const num:number= Math.sqrt(295)
+// console.log(num)
+// 
+const num1:number= Math.sqrt(278)
+console.log(num1)
+
+//** */ Q 100/
+
+
+function getRandom():number{
+  return Math.floor(Math.random()*100)+1
+}
+ console.log(getRandom())
+
+
+
+// Q no 101
+const num:number= Math.abs(-5-15)
+console.log(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
