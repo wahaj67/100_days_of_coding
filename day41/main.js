adc9e060e5298d44f47ff7581479886d155eb87b
wahaj ali <160678510+wahaj67@users.noreply.github.com>
2024-08-09 15:52:23 +0500

    Add files via upload

---
+++ day41/main.js
@@ -0,0 +1,30 @@
+"use strict";
+// Q 121
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+    console.log(i);
+}
+// Q 122
+let counter = 10;
+while (counter > 0) {
+    if (counter === 5) {
+        break;
+    }
+    console.log(counter);
+    counter--;
+}
+// Q123
+function vowel(str) {
+    const vowels = "aeiouAEIOU";
+    for (const char of str) {
+        if (vowels.includes(char)) {
+            console.log(`First vowel found: ${char}`);
+            break;
+        }
+        console.log(char);
+    }
+}
+vowel("hello");
+vowel("sidrawahajali");
