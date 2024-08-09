15a8e13476e759404e6b60374ac9ee236bd9721f
wahaj ali <160678510+wahaj67@users.noreply.github.com>
2024-08-09 15:03:13 +0500

    Add files via upload

---
+++ day39/main.ts
@@ -0,0 +1,73 @@
+// Q115
+function dayByNumber(day: number) {
+  switch (day) {
+    case 1:
+      return "Monday";
+    case 2:
+      return "tuesday";
+    case 3:
+      return "wednesday";
+    case 4:
+      return "thursday";
+    case 5:
+      return "friday";
+    case 6:
+      return "saturday";
+    case 7:
+      return "sunday";
+    default:
+      console.log("invalid day");
+  }
+}
+
+console.log(dayByNumber(5));
+// Q116
+function logSeason(month: number): string {
+  switch (month) {
+    case 12:
+    case 1:
+    case 2:
+      return "winter";
+
+    case 3:
+    case 4:
+    case 5:
+      return "spring";
+
+    case 6:
+    case 7:
+    case 8:
+      return "summer";
+
+    case 9:
+    case 10:
+    case 11:
+      return "Fall";
+    default:
+      return "invalid month";
+  }
+}
+console.log(logSeason(5))
+console.log(logSeason(10))
+
+// Q117
+
+function evaluateGrade(grade:string):string{
+    switch(grade){
+        case "A":
+            return "Excellent"
+
+        case "B":
+            return "Good"
+        case "C":
+            return "Fair"
+        case "D":
+            return "Poor" 
+        case "F":
+            return "Fail" 
+          default :
+          return "Invalid Grade"                
+    }
+}
+
+console.log(evaluateGrade("C"))
