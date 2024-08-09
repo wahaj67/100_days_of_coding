efc51fb7f52e40233e92ea34d997b7b38ede7937
wahaj ali <160678510+wahaj67@users.noreply.github.com>
2024-08-09 15:26:14 +0500

    Add files via upload

---
+++ day 40/main.js
@@ -0,0 +1,16 @@
+"use strict";
+// Q118
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+// Q119
+let count = 0;
+while (count < 5) {
+    console.log("Hello World!");
+    count++;
+}
+// Q120
+let myFavMovies = ["The Matrix", "The Godfather", "The Shawshank Redemption"];
+for (let movie in myFavMovies) {
+    console.log(movie);
+}
---
+++ day 40/main.ts
@@ -0,0 +1,21 @@
+// Q118
+for(let i=1;i<=10; i++){
+    console.log(i)
+}
+
+// Q119
+
+let count:number = 0
+
+while(count < 5){
+    console.log("Hello World!")
+    count++
+}
+
+
+// Q120
+
+let myFavMovies:string[]=["The Matrix","The Godfather","The Shawshank Redemption"]
+ for( let movie  in myFavMovies){
+console.log(movie)
+ }
