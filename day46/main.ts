020a9a7fda3ea57d16525d8921ed28f43612506e
wahaj ali <160678510+wahaj67@users.noreply.github.com>
2024-08-10 05:06:37 +0500

    Add files via upload

---
+++ day46/main.ts
@@ -0,0 +1,22 @@
+// Q136
+for(let i =1;i<=5;i++){
+    console.log(`iteration ${i} i value i `)
+}
+
+// Q137
+
+try{
+    throw new Error("something went wrong")
+} catch(error){
+    // console.log(error.message)
+}
+
+// Q138
+
+console.log("before break point")
+
+console.log(
+    "this line has a breakpoint set on it browsers developers tools "
+)
+
+console.log("after break point")
