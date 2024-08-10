e404ec57684311c41b303ea0acf736f50493edc1
wahaj ali <160678510+wahaj67@users.noreply.github.com>
2024-08-10 05:45:21 +0500

    Add files via upload

---
+++ day47/main.ts
@@ -0,0 +1,24 @@
+let count = 5
+if(count>0){
+    console.log("count is greataer than 0")
+}
+
+// function add(a,b){
+//     return a+b
+// }
+
+// Q140
+
+// let if = 6
+// console.log(if)
+
+
+// Q141
+
+async function fetch() {
+    const data = await fetch()
+
+    console.log(data)
+}
+
+fetch()
